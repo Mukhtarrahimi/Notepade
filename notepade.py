238e14e85d90f11200cf119b37aa94907489a8cd
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-10 08:11:07 +0430

    saving file function.

--- notepade.py
+++ notepade.py
@@ -1,16 +1,29 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
+# functions......................
+def saveing_file():
+    file_location = askopenfilename(
+        defaultextension=".txt",
+        filetypes = [("Text files", "*.txt"), ("All files", "*.*")]
+    )
+    if not file_location:
+        return
+    
+
+
+
+
 root = tk.Tk()
 root.title("MY NOTEPAD")
 root.rowconfigure(0, minsize=800)
 root.columnconfigure(1, minsize=800)
 
 text_edit = tk.Text(root)
-text_edit.grid(row=0, column=1)
+text_edit.grid(row=0, column=1, sticky="nsew")
 
 frame_button = tk.Frame(root, relief=tk.RAISED, bd=3)
-frame_button.grid(row=0, column=0)
+frame_button.grid(row=0, column=0, sticky="ns")
 
 button_open = tk.Button(frame_button, text="open file")
 button_open.grid(row=0, column=0, padx=5, pady=5)
