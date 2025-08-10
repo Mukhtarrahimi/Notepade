7dc51955175deafb9f97fb646813be2a304ffb99
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-10 07:54:03 +0430

    buttons code.

--- notepade.py
+++ notepade.py
@@ -7,4 +7,7 @@ root.rowconfigure(0, minsize=800)
 root.columnconfigure(1, minsize=800)
 
 text_edit = tk.Text(root)
+frame_button = tk.Frame(root, relief=tk.RAISED, bd=3)
+button_open = tk.Button(frame_button, text="open file")
+button_save = tk.Button(frame_button, text="save file")
 root.mainloop()
